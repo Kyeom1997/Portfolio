2f8876d14cffc51f3993289abc719faa3cbd0364
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-03-17 01:30:28 +0900

    JS_Arrowup

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -12,53 +12,54 @@ document.addEventListener('scroll', () => {
 })
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
-const target = event.target;
-const link = target.dataset.link;
-if (link == null) {
-    return;
-}
-navbarMenu.classList.remove("open");
-scrollIntoView(link);
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
 });
 
-function scrollIntoView(e) {
-const scrollTo = document.querySelector(e);
-scrollTo.scrollIntoView({ behavior: "smooth" });
+// Handle Clik on "contact me" button home
 
-};
-
-const contactMe = document.querySelector(".home__contact");
-contactMe.addEventListener("click", () => {
-    const scrollTo = document.querySelector('#contact');
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-if(link == null) {
-    return;
-}
-contactMe.classList.remove("open");
-scrollIntoView(link);
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+    scrollIntoView('#contact');
 });
 
-
-// Make home slowly foad to transparent as the window scrolls down
+// Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
 
 
 
 
 
-
-
-
-
+// function about scroll smoothly
 function scrollIntoView(e) {
-const scrollTo = document.querySelector(e);
-scrollTo.scrollIntoView({ behavior: "smooth" });
-}  
-
+    const scrollTo = document.querySelector(e);
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
