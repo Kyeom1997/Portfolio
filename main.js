f274a1b94412e97a3170f0a29015dbf33bd0a14d
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2021-03-17 14:48:50 +0900

    JS_remove active from nav

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
+
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
@@ -61,6 +62,14 @@ workBtnContainer.addEventListener('click', (e) => {
     if (filter == null) {
         return;
     }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target =
+        e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
 
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
